db640ee9967cadf440dedac2ce796110dbce65ac
Corey Frenette <corey@narwhunderful.com>
2015-09-13 00:30:56 -0300

    Add template finder

---
+++ src/RemodelTemplateFinder.php
@@ -0,0 +1,16 @@
+<?php namespace BapCat\Remodel;
+
+use BapCat\Persist\Drivers\Filesystem\FilesystemDriver;
+use BapCat\Tailor\PersistTemplateFinder;
+
+class RemodelTemplateFinder extends PersistTemplateFinder {
+  private $templates;
+  private $compiled;
+  
+  public function __construct(Directory $compiled) {
+    $filesystem = new FilesystemDriver(__DIR__ . '/../templates');
+    $templates  = $persist->get('/');
+    
+    parent::__construct($templates, $compiled);
+  }
+}
