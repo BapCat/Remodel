014c862937918040ddc91fce8325804d2c6425cb
Corey Frenette <corey@narwhunderful.com>
2015-09-13 16:22:24 -0300

    Remodel shouldn't require a full instance of Tailor

--- src/Registry.php
+++ src/Registry.php
@@ -1,5 +1,7 @@
 <?php namespace BapCat\Remodel;
 
+use BapCat\Interfaces\Ioc\Ioc;
+use BapCat\Tailor\Compilers\Compiler;
 use BapCat\Tailor\Tailor;
 use ICanBoogie\Inflector;
 
@@ -8,8 +10,8 @@ class Registry {
   
   private $tailor;
   
-  public function __construct(Tailor $tailor) {
-    $this->tailor = $tailor;
+  public function __construct(Ioc $ioc, RemodelTemplateFinder $finder, Compiler $compiler) {
+    $this->tailor = $ioc->make(Tailor::class, [$finder, $compiler]);
     
     if(!self::$globalFunctionsRegistered) {
       self::$globalFunctionsRegistered = true;
