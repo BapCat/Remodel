281313514871bcbbb4517ea89c0b80da69aa80d0
Corey Frenette <corey@narwhunderful.com>
2015-10-19 10:34:28 -0300

    Start working on moving templates to nom

--- templates/Entity.php
+++ templates/Entity.php
@@ -1,10 +1,8 @@
-<<?= '?php' ?> namespace <?= $namespace ?>;
+@php namespace {! $namespace !};
 
 <?php
 
 use BapCat\Remodel\EntityDefinitionOptions;
-
-//use function BapCat\Remodel\titlize;
 
 if(!function_exists('defToParam')) {
   function defToParam(EntityDefinitionOptions $def, $nullable = false) {
@@ -64,75 +62,75 @@ if(!function_exists('defToParam')) {
 
 ?>
 
-class <?= $name ?> implements \BapCat\Remodel\Entity, \JsonSerializable {
+class {! $name !} implements \BapCat\Remodel\Entity, \JsonSerializable {
   use \BapCat\Propifier\PropifierTrait;
   
-<?php foreach(array_merge([$id], $required, $optional) as $def): ?>
-  private $<?= $def->alias ?>;
-<?php endforeach; ?>
-<?php foreach($virtual as $def): ?>
-  private $<?= $def['alias'] ?>;
-<?php endforeach; ?>
+@each(array_merge([$id], $required, $optional) as $def)
+  private ${! $def->alias !};
+@endeach
+@each($virtual as $def)
+  private ${! $def['alias'] !};
+@endeach
   
-  private function __construct() { }
+  private @func(__construct)
   
-  public static function create(<?= defsToParams($required) ?>) {
-    return self::make(null, <?= defsToArgs($required) ?>);
-  }
+  public static @func(create({! defsToParams($required) !}))
+    return self::make(null, {! defsToArgs($required) !});
+  @endfunc
   
-  public static function from(<?= defsToParams(array_merge([$id], $required)) ?>) {
-    return self::make(<?= defsToArgs(array_merge([$id], $required)) ?>);
-  }
+  public static @func(from({! defsToParams(array_merge([$id], $required)) !}))
+    return self::make({! defsToArgs(array_merge([$id], $required)) !});
+  @endfunc
   
-  public static function fromRepository(<?= defsToParams(array_merge([$id], $required)) ?>, callable $accessor) {
-    $entity = static::from(<?= defsToArgs(array_merge([$id], $required)) ?>);
-    $accessor(<?= virtualsToArgs($virtual) ?>);
+  public static @func(fromRepository({! defsToParams(array_merge([$id], $required)) !}, callable $accessor))
+    $entity = static::from({! defsToArgs(array_merge([$id], $required)) !});
+    $accessor({! virtualsToArgs($virtual) !});
     return $entity;
-  }
+  @endfunc
   
-  private static function make(<?= defsToParams(array_merge([$id], $required, $optional), true) ?>) {
-    $entity = new \<?= $namespace ?>\<?= $name ?>();
-<?php foreach(array_merge([$id], $required, $optional) as $def): ?>
-    $entity-><?= $def->alias ?> = $<?= $def->alias ?>;
-<?php endforeach; ?>
+  private static @func(make({! defsToParams(array_merge([$id], $required, $optional), true) !}))
+    $entity = new \{! $namespace !}\{! $name !}();
+@each(array_merge([$id], $required, $optional) as $def)
+    $entity->{! $def->alias !} = ${! $def->alias !};
+@endeach
     
     return $entity;
-  }
-<?php foreach(array_merge([$id], $required, $optional) as $def): ?>
+  @endfunc
+@each(array_merge([$id], $required, $optional) as $def)
   
-  protected function get<?= \BapCat\Remodel\titlize($def->alias) ?>() {
-    return $this-><?= $def->alias ?>;
-  }
+  protected @func(get{! @titleize($def->alias) !})
+    return $this->{! $def->alias !};
+  @endfunc
   
-  protected function set<?= \BapCat\Remodel\titlize($def->alias) ?>(<?= defToParam($def) ?>) {
-    $this-><?= $def->alias ?> = $<?= $def->alias ?>;
-  }
-<?php endforeach; ?>
-<?php foreach($virtual as $def): ?>
+  protected @func(set{! @titleize($def->alias) !}({! defToParam($def) !}))
+    $this->{! $def->alias !} = ${! $def->alias !};
+  @endfunc
+@endeach
+@each($virtual as $def)
   
-  protected function get<?= \BapCat\Remodel\titlize($def['alias']) ?>() {
-    return $this-><?= $def['alias'] ?>;
-  }
-<?php endforeach; ?>
+  protected @func(get{! @titleize($def['alias']) !})
+    return $this->{! $def['alias'] !};
+  @endfunc
+@endeach
   
-  public function __toString() {
-    $output = '<?= $namespace ?>\<?= $name ?> ';
+  public @func(__toString)
+    $output = '{! $namespace !}\{! $name !} ';
     
     if($this->id === null) {
       return $output . '(new)';
     }
     
     return $output . $this->id;
-  }
+  @endfunc
   
-  public function jsonSerialize() {
+  public @func(jsonSerialize)
     return [
-<?php foreach(array_merge([$id], $required, $optional) as $def): ?>
-      '<?= $def->alias ?>' => $this-><?= $def->alias ?>,
-<?php endforeach; ?>
-<?php foreach($virtual as $def): ?>
-      '<?= $def['alias'] ?>' => $this-><?= $def['alias'] ?>,
-<?php endforeach; ?>
+@each(array_merge([$id], $required, $optional) as $def)
+      '{! $def->alias !}' => $this->{! $def->alias !},
+@endeach
+@each($virtual as $def)
+      '{! $def['alias'] !}' => $this->{! $def['alias'] !},
+@endeach
     ];
-  }
+  @endfunc
 }
